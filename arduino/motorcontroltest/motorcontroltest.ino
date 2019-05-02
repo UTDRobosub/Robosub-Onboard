ef59ad321f03d78f11fb8d88402a3c7cf6f68412
Ezra Anderson <ezradaleanderson@hotmail.com>
2019-05-02 18:53:41 -0500

    add arm servos

--- arduino/motorcontroltest/motorcontroltest.ino
+++ arduino/motorcontroltest/motorcontroltest.ino
@@ -45,7 +45,6 @@ byte verticalMotorSign = 0;
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-
 #include <Servo.h>
 
 byte servoPin11 = 11;
@@ -53,10 +52,19 @@ byte servoPin10 = 10;
 byte servoPin9 = 9;
 byte servoPin8 = 8;
 
+//arm motors
+byte servoPin2 = 2;
+byte servoPin3 = 3;
+byte servoPin4 = 4;
+
 Servo servo;
 Servo servo10;
 Servo servo9;
 Servo servo8;
+
+Servo servo2;
+Servo servo3;
+Servo servo4;
 
 void motor_setup() {
   pinMode(verticalMotorPin1, OUTPUT);
@@ -68,11 +76,17 @@ void motor_setup() {
   servo10.attach(servoPin10);
   servo9.attach(servoPin9);
   servo8.attach(servoPin8);
+  servo2.attach(servoPin2);
+  servo3.attach(servoPin3);
+  servo4.attach(servoPin4);
 
   servo.writeMicroseconds(1500); // send "stop" signal to ESC.
   servo10.writeMicroseconds(1500); // send "stop" signal to ESC.
   servo9.writeMicroseconds(1500); // send "stop" signal to ESC.
   servo8.writeMicroseconds(1500);
+  servo2.write(30);
+  servo3.write(30);
+  servo4.write(30);
   delay(1000); // delay to allow the ESC to recognize the stopped signal
 }
 
@@ -100,11 +114,14 @@ void verticalMotorWrite(int value){ //[-255,255]
 //Main program
 
 struct MotorValues {
-    short bl;
-    short br;
-    short ul;
-    short ur;
-    short v;
+  short bl;
+  short br;
+  short ul;
+  short ur;
+  short v;	//-255 to 255
+  short a1;	//0 to 130 degrees
+	short a2;	//0 to 180 degrees
+	short a3;	//0 to 65 degrees
 };
 
 MotorValues motorvals;
@@ -116,6 +133,9 @@ void motor_update(){
   servo9 .writeMicroseconds(motorvals.bl); //actually bl
   servo10.writeMicroseconds(motorvals.ur); //actually fr
   verticalMotorWrite(motorvals.v);         //vertical motors
+  servo2.write(motorvals.a1); 			   //arm motors
+  servo3.write(motorvals.a2); 
+  servo4.write(motorvals.a3); 
 }
 
 void receiveMessageCallback(void* instance, char* message, int length, bool needsresponse, char** response, int* responselength){
@@ -130,6 +150,10 @@ void setup(){
   motorvals.br = 1500;
   motorvals.ul = 1500;
   motorvals.ur = 1500;
+  motorvals.v = 0;
+  motorvals.a1 = 30;
+  motorvals.a2 = 30;
+  motorvals.a3 = 30;
   
   serial_setup();
   motor_setup();
@@ -144,4 +168,3 @@ void loop(){
   
   delay(1);
 }
-
